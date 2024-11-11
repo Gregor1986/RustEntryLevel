c0f441e0d54967d53bccd7b4d8f43d38ed012ad5
Gregor1986 <egor953@yandex.ru>
2024-11-11 18:40:06 +0300

    task step_4_1_5 - OK

--- step_4_1_5/src/main.rs
+++ step_4_1_5/src/main.rs
@@ -1,3 +1,12 @@
+// Сумматор
+// Напишите программу, которая считывает два целых числа и выводит их сумму.
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut input_string = String::new();
+    let mut input_string_1 = String::new();
+    io::stdin().read_line(&mut input_string).expect("Не удалось прочитать строку");
+    let a = input_string.trim().parse::<i32>().expect("Это не число");
+    io::stdin().read_line(&mut input_string_1).expect("Не удалось прочитать строку");
+    let b = input_string_1.trim().parse::<i32>().expect("Это не число");
+    println!("{}", a + b);
 }
