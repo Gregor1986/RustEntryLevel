0a11176b96eabbc2e20c5c171a6912c4b4d3e00b
Gregor1986 <egor953@yandex.ru>
2024-11-11 19:07:56 +0300

    task step_4_1_15 - OK

--- step_4_1_15/src/main.rs
+++ step_4_1_15/src/main.rs
@@ -1,3 +1,34 @@
+// Арифметика систем
+// Напишите программу, которая считывает два целых числа с консоли в десятичной системе и выводит в двоичной, 
+// восьмеричной, шестнадцатеричной системах арифметические операции (+, -, *, /, %) над первым числом.
+// Уточним, что каждая арифметическая операция должна быть выведена в каждой из систем счисления. При выводе 
+// операций для следующей системы счисления используйте переход на новую строку. Для ясности посмотрите на 
+// входные и выходные данные. Гарантируется, что второе число не равно 0!
+use std::io;
 fn main() {
-    println!("Hello, world!");
-}
+    let mut input_string = String::new();
+    io::stdin().read_line(&mut input_string).expect("Не удалось прочитать строку");
+    let a = input_string.trim().parse::<i32>().expect("Это не число"); 
+    let mut input_string_1 = String::new();
+    io::stdin().read_line(&mut input_string_1).expect("Не удалось прочитать строку");
+    let b = input_string_1.trim().parse::<i32>().expect("Это не число"); 
+    println!("{:#b} + {:#b} = {:#b}", a, b, a + b);
+    println!("{:#o} + {:#o} = {:#o}", a, b, a + b);
+    println!("{:#x} + {:#x} = {:#x}", a, b, a + b);
+    println!();
+    println!("{:#b} - {:#b} = {:#b}", a, b, a - b);
+    println!("{:#o} - {:#o} = {:#o}", a, b, a - b);
+    println!("{:#x} - {:#x} = {:#x}", a, b, a - b);
+    println!();
+    println!("{:#b} * {:#b} = {:#b}", a, b, a * b);
+    println!("{:#o} * {:#o} = {:#o}", a, b, a * b);
+    println!("{:#x} * {:#x} = {:#x}", a, b, a * b);
+    println!();
+    println!("{:#b} / {:#b} = {:#b}", a, b, a / b);
+    println!("{:#o} / {:#o} = {:#o}", a, b, a / b);
+    println!("{:#x} / {:#x} = {:#x}", a, b, a / b);
+    println!();
+    println!("{:#b} % {:#b} = {:#b}", a, b, a % b);
+    println!("{:#o} % {:#o} = {:#o}", a, b, a % b);
+    println!("{:#x} % {:#x} = {:#x}", a, b, a % b);
+} 
