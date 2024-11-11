e3d2453d051d0a5452d7b95d152feb913a1ffc30
Gregor1986 <egor953@yandex.ru>
2024-11-11 18:21:58 +0300

    task step_3_4_10 - OK

--- step_3_4_10/src/main.rs
+++ step_3_4_10/src/main.rs
@@ -1,3 +1,8 @@
+// Напишите программу, которая считывает имя и затем выводит приветственное сообщение с использованием этого имени:
+// Привет, {имя}!
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut input_string = String::new();
+    io::stdin().read_line(&mut input_string).expect("Не удалось прочитать строку");
+    println!("Привет, {}!", input_string.trim());
 }
