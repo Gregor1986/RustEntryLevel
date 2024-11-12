fe20ccb9f1b84ee36f38746d5cc120081ce34082
Gregor1986 <egor953@yandex.ru>
2024-11-12 14:00:31 +0300

    task step_4_4_5 - OK

--- step_4_4_5/src/main.rs
+++ step_4_4_5/src/main.rs
@@ -1,3 +1,11 @@
+// Смайл Фейс
+// Напишите программу, которая считывает строку, представленная в виде эмодзи и выводит его обратно следующим 
+// образом:
+// Мое настроение: эмодзи
+use std::io;
 fn main() {
-    println!("Hello, world!");
-}
+    let mut input_string = String::new();
+    io::stdin().read_line(&mut input_string).expect("Не удалось прочитать строку");
+    let a = input_string.trim().parse::<char>().expect("Это не число"); 
+    println!("Мое настроение: {}", a);
+} 
