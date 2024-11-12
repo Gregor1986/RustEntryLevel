90b66b68e4b152a5a5dabcbe654ec797f59914f6
Gregor1986 <egor953@yandex.ru>
2024-11-12 13:42:27 +0300

    task 4_2_11 - OK

--- step_4_2_11/src/main.rs
+++ step_4_2_11/src/main.rs
@@ -1,3 +1,14 @@
+// Копейка рубль бережёт 
+// Напишите программу, которая считывает вещественное число и выводит его целое и дробную часть (до 3 знаков).
+// Например, если введено число 123.456789, программа должна вывести:
+// 123
+// 0.457
+use std::io;
 fn main() {
-    println!("Hello, world!");
-}
+    let mut input_string = String::new();
+    io::stdin().read_line(&mut input_string).expect("Не удалось прочитать строку");
+    let a = input_string.trim().parse::<f64>().expect("Это не число"); 
+    let c = a as i32;
+    println!("{}", c);
+    println!("{:.3}", a - c as f64);
+} 
